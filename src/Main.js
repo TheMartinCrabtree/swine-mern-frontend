6c31adfdb57bbeb4dd1804e6feae689290467654
MC-Games\MC <martin.crabtree@gmail.com>
2020-03-24 13:38:27 -0400

    setting up testing routes

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,23 @@
-import react from "react";
+import React, { useEffect, useState } from "react";
 
 const Main=()=>{
+    let charFormat = { 
+        name_first: "",
+        name_family: "",
+        age: null,
+     };
+    const [ characterArr, setCharacterArr ] = useState([]);
+    const [ formCharacter, setFormCharacter ] = useState(charFormat);
+
+    // useEffect(()=>{
+    //     fetch("http://localhost:3003/api/characters")
+    //         .then(response=>response.json())
+    //         .then(json=>console.log(json));
+
+
+    // }, [])
+
+
     return(
         <main>
             <h3> This is Main </h3>
